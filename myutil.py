d97bff13306ac36fdcfb77b90360d19a80d712cb
fumionakai <iikaofumio-n219@outlook.jp>
2022-07-22 19:37:24 +0900

    @ fumionakai バグを見つけました。曜日が英語でフォーマットされています。 Fixes #3

--- myutil.py
+++ myutil.py
@@ -6,7 +6,11 @@ def get_time():
 def get_formatted_date(t=None):
     if not t:
         t = datetime.now()
-    d = t.strftime('%Y年%m月%d日（%a）')
+    d_week = {'Sun': '日', 'Mon': '月', 'Tue': '火', 'Wed': '水',
+          'Thu': '木', 'Fri': '金', 'Sat': '土'}
+    key = t.strftime('%a')
+    w = d_week[key]
+    d = t.strftime('%Y年%m月%d日') + f'（{w}）' #f'{now:%Y年%m月%d日}（{w}）'
     return d
 
 print(get_formatted_date())
